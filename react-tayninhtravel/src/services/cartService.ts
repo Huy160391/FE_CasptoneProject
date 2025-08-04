e674d0181f14d1442453c03ab3f72c47a0c4e086
Huy160391 <huynqse160391@gmail.com>
2025-08-04 11:29:13 +0700

    fix

--- react-tayninhtravel/src/services/cartService.ts
+++ react-tayninhtravel/src/services/cartService.ts
@@ -48,7 +48,7 @@ export const checkoutCart = async (
         '/Product/checkout',
         {
             cartItemIds,
-            myVoucherCodeId,
+            myVoucherCodeId: myVoucherCodeId || null,
         },
         {
             headers: {
